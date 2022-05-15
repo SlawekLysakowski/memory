f34fd4550ef2124a2e887c76b990af1e9fe1e544
SlawekLysakowski <slawku1@gmail.com>
2022-05-15 12:34:57 +0200

    delete category

--- src/app/ui/category-box/category-box.component.ts
+++ src/app/ui/category-box/category-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { faGear, faPlay, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
@@ -12,7 +12,12 @@ export class CategoryBoxComponent implements OnInit {
   faPlay = faPlay;
 
   @Input() categoryName = "";
+  @Output() removeEvent: EventEmitter<void> = new EventEmitter<void>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  emitRemove() {
+    this.removeEvent.emit();
+  }
 }
