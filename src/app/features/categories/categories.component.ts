c246b95539bedd683b50abca75f10c3757897810
SlawekLysakowski <slawku1@gmail.com>
2022-05-15 14:45:17 +0200

    Category servis

--- src/app/features/categories/categories.component.ts
+++ src/app/features/categories/categories.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Category } from "../../models/category-model";
+import { CategoriesService } from "../../services/categories.service";
 
 @Component({
   selector: "app-categories",
@@ -10,17 +11,38 @@ import { Category } from "../../models/category-model";
 export class CategoriesComponent implements OnInit {
   faPlus = faPlus;
   newCategoryName = "";
-  categories: Category[] = [{ name: "Dom" }, { name: "Rodzina" }];
-  constructor() {}
+  categories: Category[] = [];
 
-  ngOnInit(): void {}
+  constructor(private categoriesService: CategoriesService) {}
+
+  ngOnInit(): void {
+    this.categoriesService
+      .getCategories()
+      .subscribe(categories => (this.categories = categories));
+  }
 
   addCategory() {
-    this.categories.push({ name: this.newCategoryName });
-    this.newCategoryName = "";
+    this.categoriesService
+      .addCategory({ name: this.newCategoryName })
+      .subscribe(category => {
+        this.categories.push(category);
+        this.newCategoryName = "";
+      });
   }
 
   removeCategory(category: Category) {
-    this.categories.splice(this.categories.indexOf(category), 1);
+    if (category.id) {
+      this.categoriesService
+        .removeCategory(category.id)
+        .subscribe(() =>
+          this.categories.splice(this.categories.indexOf(category), 1)
+        );
+    }
+  }
+
+  addCategoryKey($event: KeyboardEvent) {
+    if ($event.key == "enter") {
+      this.addCategory();
+    }
   }
 }
